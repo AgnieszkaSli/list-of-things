00abe377b51791b5e4a2265f1a67d73cbb6c96f9
AgnieszkaSli <agnieszka316@wp.pl>
2018-07-08 12:15:43 +0200

    add dependencies; change @RequestMapping

--- src/main/java/com/aga/listofthings/MainController.java
+++ src/main/java/com/aga/listofthings/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
-@RequestMapping("/demo")
+@RequestMapping("/list")
 public class MainController {
 
     @ResponseBody
